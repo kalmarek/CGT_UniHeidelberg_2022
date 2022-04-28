c1a1fd85a20c55b3316fa08763c834887c5d9fc2
Marek Kaluba <kalmar@amu.edu.pl>
2022-04-28 15:20:08 +0200

    add the main file of the module

---
+++ src/CGT_UniHeidelberg_2022.jl
@@ -0,0 +1,9 @@
+module CGT_UniHeidelberg_2022
+
+abstract type GroupElement end
+
+include("orbits.jl")
+include("AbstractPermutations.jl")
+using .AbstractPermutations
+
+end # of module CGT_UniHeidelberg_2022

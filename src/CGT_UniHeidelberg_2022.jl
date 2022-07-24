5cc2f7631308f2c3bfad86578e7850b9f9037389
Ferdinand Vanmaele <Vanmaele@stud.uni-heidelberg.de>
2022-07-24 22:05:15 +0200

    CGT: include Alphabet, Word, FreeGroup

--- src/CGT_UniHeidelberg_2022.jl
+++ src/CGT_UniHeidelberg_2022.jl
@@ -16,5 +16,8 @@ using .AbstractPermutations
 include("permutations.jl")
 include("permutations_02.jl")
 include("schreier_sims.jl")
+include("alphabet.jl")
+include("word.jl")
+include("free_group.jl")
 
 end # of module CGT_UniHeidelberg_2022

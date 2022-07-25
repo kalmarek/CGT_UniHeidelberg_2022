07a1880620d30af69d30b1ed2cb86f1a5d9bac77
Ferdinand Vanmaele <Vanmaele@stud.uni-heidelberg.de>
2022-07-25 15:12:16 +0200

    Base.iterate(::PermutationGroup) using backtrack!

--- src/backtrack.jl
+++ src/backtrack.jl
@@ -2,12 +2,12 @@
 
 const CGT = CGT_UniHeidelberg_2022
 
-export backtrack!, backtrack_stack!
+export backtrack!
 
 """ Basic version of backtrack search without oracle.
     Returns a list of all elements of G.
 """
-function backtrack!(L::AbstractVector, sc::CGT.StabilizerChain;
+function backtrack!(sc::CGT.StabilizerChain, L::AbstractVector;
                     g::CGT.Permutation = CGT.Permutation(Int[]), depth::Int = 1)
     T = CGT.transversal(sc, depth)
 
@@ -15,27 +15,48 @@ function backtrack!(L::AbstractVector, sc::CGT.StabilizerChain;
         if CGT.depth(sc) == depth  # we are in a leaf node
             push!(L, g*T[δ])
         else
-            backtrack!(L, sc, g=g*T[δ], depth=depth+1)
+            backtrack!(sc, L, g=g*T[δ], depth=depth+1)
         end
     end
     return L
 end
 
-# XXX: Version using stack for experimentation purposes
-function backtrack_stack!(L::AbstractVector, sc::CGT.StabilizerChain)
-    stack = [(1, CGT.Permutation(Int[]))]
+function backtrack!(sc::CGT.StabilizerChain, C::Channel;
+                    g::CGT.Permutation = CGT.Permutation(Int[]), depth::Int = 1)
+    T = CGT.transversal(sc, depth)
 
-    while !isempty(stack)
-        depth, g = pop!(stack)
-        T = CGT.transversal(sc, depth)
-
-        for δ ∈ T
-            if CGT.depth(sc) == depth  # we are in a leaf node
-                push!(L, g*T[δ])
-            else
-                push!(stack, (depth+1, g*T[δ]))
-            end
+    for δ ∈ T
+        if CGT.depth(sc) == depth  # we are in a leaf node
+            put!(C, g*T[δ])
+        else
+            backtrack!(sc, C, g=g*T[δ], depth=depth+1)
         end
     end
-    return L
+end
+
+# Helpers to make backtrack! work with iterator interface
+struct PGroupIterator
+    C::Channel
+
+    function PGroupIterator(G::CGT.PermutationGroup)
+        C = Channel((channel_arg) ->
+            backtrack!(CGT.stabilizer_chain(G), channel_arg))
+        return new(C)
+    end
+end
+
+Base.take!(It::PGroupIterator) = take!(It.C)
+Base.isready(It::PGroupIterator) = isready(It.C)
+
+function Base.iterate(G::CGT.PermutationGroup)
+    state = PGroupIterator(G)
+    return (take!(state), state) # should contain at least one element
+end
+
+function Base.iterate(::CGT.PermutationGroup, state::PGroupIterator)
+    if isready(state)
+        return (take!(state), state)
+    else
+        return nothing
+    end
 end

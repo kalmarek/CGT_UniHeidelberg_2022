cd355ebc632b1a0b9f62c6d1251d256343725c72
Ferdinand Vanmaele <Vanmaele@stud.uni-heidelberg.de>
2022-07-25 15:50:45 +0200

    add backtrack_stack!

--- src/backtrack.jl
+++ src/backtrack.jl
@@ -2,7 +2,7 @@
 
 const CGT = CGT_UniHeidelberg_2022
 
-export backtrack!
+export backtrack!, backtrack_stack!
 
 """ Basic version of backtrack search without oracle.
     Returns a list of all elements of G.
@@ -32,6 +32,26 @@ function backtrack!(sc::CGT.StabilizerChain, C::Channel;
             backtrack!(sc, C, g=g*T[δ], depth=depth+1)
         end
     end
+end
+
+# Experimental version using explicit stack
+# XXX: the ordering is different from the recursive version
+function backtrack_stack!(sc::CGT.StabilizerChain, L::AbstractVector)
+    stack = [(1, CGT.Permutation(Int[]))]
+
+    while !isempty(stack)
+        depth, g = pop!(stack)
+        T = CGT.transversal(sc, depth)
+
+        for δ ∈ T
+            if CGT.depth(sc) == depth  # we are in a leaf node
+                push!(L, g*T[δ])
+            else
+                push!(stack, (depth+1, g*T[δ]))
+            end
+        end
+    end
+    return L
 end
 
 # Helpers to make backtrack! work with iterator interface
